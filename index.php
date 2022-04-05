f3091a54c5a2292334be20a591f8afc3e81aa2b5
avila nicolas <aviladanielnicolas@gmail.com>
2022-04-05 17:10:22 -0300

    pase todo a un php y mejora del formulario

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Sslider</title>
+</head>
+
+<body>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="foto1" >Clikea para ingresar la primera foto del Slider</label>
+        <input class="ingresa" accept="image/png,image/jpeg" type="file" id="foto1" required >
+        <label for="foto2">Clikea para ingresar la segunda foto del Slider</label>
+        <input class="ingresa" accept="image/png,image/jpeg" type="file" id="foto2"  required>
+        <label for="foto3">Clikea para ingresar tercera foto del Slider</label>
+        <input class="ingresa" accept="image/png,image/jpeg" type="file" id="foto3" required>
+        
+        <input type="button" value="Crear" name="boto" id="boto">
+    </form>
+    
+
+</body>
+
+</html>
